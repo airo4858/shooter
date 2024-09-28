1c5907ac89d4ef24c4bd6607147d5a4696da3308
Aidan Rogers <143754636+airo4858@users.noreply.github.com>
2024-09-27 20:18:30 -0600

    Made the enemy shoot function, but not have an effect on the player, i.e. it doesn;t do anything

--- Actors/enemy_2.gd
+++ Actors/enemy_2.gd
@@ -12,6 +12,6 @@ func hit(damage_number: int):
 		var new_enemy2 = small_enemy.instantiate()
 		get_parent().add_child(new_enemy1)
 		get_parent().add_child(new_enemy2)
-		new_enemy1.position = position + Vector2(50,50)
-		new_enemy2.position = position + Vector2(-50,-50)
+		new_enemy1.position = position + Vector2(15,15)
+		new_enemy2.position = position + Vector2(-15,-15)
 		get_tree().get_root().get_node("Main/HUD").add_score(10)
--- Actors/shooting.gd
+++ Actors/shooting.gd
@@ -1,28 +1,30 @@
 extends State
 
 @onready var target: CharacterBody2D
-@onready var enemy: CharacterBody2D
-var projectile: CharacterBody2D
+var projectile: Area2D
 var is_chasing : bool = true
 var shoot_time: float = 1.5
+var projectile_speed: float = 100.0
 @export var enemy_projectile: Resource
 
 
 func initialize():
 	target = get_node("Main/Player")
-	enemy = get_node("Main/Enemy")
-	projectile = enemy_projectile.instantiate()
 
 func process_state(delta: float):
 	#print("I shitted my pants")
 	is_chasing = false
-	#aim_and_shoot()
+	aim_and_shoot()
 	
 func aim_and_shoot():
 	look_at(target.position)
 	shoot()
 
 func shoot():
-	get_parent().add_child(projectile)  # Add projectile to the scene
-	enemy_projectile.position = enemy.position  # Set the starting position
-	enemy_projectile.direction = (target.position - enemy.position).normalized()
+	var new_projectile = enemy_projectile.instantiate()
+	var direction = (target.position - body.position).normalized()
+	get_parent().add_child(new_projectile)  # Add projectile to the scene
+	var projectile_forward = body.position.direction_to(target.position)
+	#var projectile_forward = Vector2.from_angle(rotation)
+	new_projectile.fire(projectile_forward, projectile_speed)
+	new_projectile.position = body.position  # Set the starting position
